1fc4600dbb196a3e9bfe54c4707151dd6f243117
José Antonio Rodríguez <jarodriguez@1551452.no-reply.drupal.org>
2017-03-10 20:23:48 +0000

    fixing spaces and arrays

--- src/Form/CecAdminInvalidateUrl.php
+++ src/Form/CecAdminInvalidateUrl.php
@@ -36,14 +36,14 @@ class CecAdminInvalidateUrl extends ConfigFormBase {
       '#type' => 'textarea',
       '#title' => $this->t('URL to invalidate'),
       '#description' => $this->t('Specify the existing path you wish to invalidate. For example: /node/28, /forum/1. Enter one value per line'),
-      '#required' => TRUE
+      '#required' => TRUE,
     ];
 
-    $form['invalidate'] = array(
+    $form['invalidate'] = [
       '#type' => 'submit',
       '#value' => $this->t('Invalidate URL'),
-      '#submit' => array('::invalidateSubmission'),
-    );
+      '#submit' => ['::invalidateSubmission'],
+    ];
 
     return $form;
   }
@@ -53,11 +53,11 @@ class CecAdminInvalidateUrl extends ConfigFormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
 
-    // Get the URL
+    // Get the URL.
     $url_value = explode("\n", $form_state->getValue('url'));
 
     if (!empty($url_value) && is_array($url_value) && count($url_value) > 0) {
-      foreach($url_value as $value) {
+      foreach ($url_value as $value) {
         if (substr($value, 0, 1) != '/' && !empty($value)) {
           $form_state->setErrorByName('url', $this->t('The URL introduced is not valid.'));
         }
@@ -73,27 +73,27 @@ class CecAdminInvalidateUrl extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function invalidateSubmission(array &$form, FormStateInterface $form_state) {
-    // Get the URL
+    // Get the URL.
     $url_value = explode("\n", $form_state->getValue('url'));
 
-    // Get the AWS Credentials
+    // Get the AWS Credentials.
     $config = \Drupal::config('cec.settings');
 
-    // Check if the credentials are configured
+    // Check if the credentials are configured.
     if (!$config->get('cec_region' && !$config->get('cec_key') && !$config->get('cec_secret'))) {
       drupal_set_message($this->t('You must configure the Global Settings correctly before execute an invalidation.'), 'error');
     }
 
     else {
-      // Get the Paths
-      $paths = array();
+      // Get the Paths.
+      $paths = [];
       foreach ($url_value as $value) {
         if ($value) {
           $paths[] = trim($value);
         }
       }
 
-      // Invalidate URL
+      // Invalidate URL.
       list($status, $message) = cloudfront_edge_caching_invalidate_url($paths);
 
       if ($status == TRUE) {
